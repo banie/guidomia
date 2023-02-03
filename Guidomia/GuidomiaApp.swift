093b0f3a21bb14545efcbf386b41d2a74de62c6d
Banie Setijoso <banie.setijoso@gmail.com>
2023-02-03 15:18:34 -0500

    Initial Commit
    (cherry picked from commit 1e66073a0586d1cb753b2ddb8f453f53b6ab7b16)

---
+++ Guidomia/GuidomiaApp.swift
@@ -0,0 +1,20 @@
+//
+//  GuidomiaApp.swift
+//  Guidomia
+//
+//  Created by Banie Setijoso on 2023-02-03.
+//
+
+import SwiftUI
+
+@main
+struct GuidomiaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
