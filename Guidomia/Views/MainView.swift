bdc7fad9ba962a12bca11d6c37474a2c9bd6fdfb
Banie Setijoso <banie.setijoso@gmail.com>
2023-02-03 16:48:35 -0500

    Rearranging some folders. Put the json file here as well. Note there's some persistence/core data logic here given by apple, but we'll use it later once reaching level 3
    (cherry picked from commit 0e374afd1ea716c936512524c43e9c020a62ec1a)

---
+++ Guidomia/Views/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Guidomia
+//
+//  Created by Banie Setijoso on 2023-02-03.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
