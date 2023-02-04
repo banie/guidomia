d99a300072f749f519109f7a74fc05ffb5257d69
Banie Setijoso <banie.setijoso@gmail.com>
2023-02-04 16:10:01 -0500

    Implement the expandable and collapsable on the list as it gets tapped. Expand when tap and collapse the others, and show the list of pro and cons

--- Guidomia/Entities/CarDetail.swift
+++ Guidomia/Entities/CarDetail.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct CarDetail: Codable {
+struct CarDetail: Codable, Equatable {
     let model: String
     let make: String
     let customerPrice: Double
--- Guidomia/Views/CarListView.swift
+++ Guidomia/Views/CarListView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct CarListView: View {
-    let presenter = CarListPresenter()
+    @ObservedObject var presenter = CarListPresenter()
     
     var body: some View {
         NavigationView {
@@ -28,38 +28,85 @@ struct CarListView: View {
                             maxHeight: 200
                         )
                     List {
-                        let carList = presenter.carList
-                        ForEach(carList) { carDetail in
-                            HStack {
-                                Image(carDetail.make + " - " + carDetail.model)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(
-                                        maxWidth: 150,
-                                        maxHeight: 50
-                                    )
-                                VStack(spacing: 2) {
-                                    Text(carDetail.make + " - " + carDetail.model)
-                                        .font(.headline)
-                                        .foregroundColor(.secondary)
+                        ForEach(presenter.items) { item in
+                            VStack {
+                                HStack {
+                                    Image(item.detail.make + " - " + item.detail.model)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(
+                                            maxWidth: 150,
+                                            maxHeight: 50
+                                        )
+                                    VStack(spacing: 2) {
+                                        Text(item.detail.make + " - " + item.detail.model)
+                                            .font(.title2)
+                                            .foregroundColor(.secondary)
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                        Text(String(format: "Price: %@k", String(Int(item.detail.customerPrice / 1000))))
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                        HStack(spacing: 2) {
+                                            let numOfStars: Int = item.detail.rating
+                                            ForEach(1...numOfStars, id: \.self) { _ in
+                                                Image(systemName: "star.fill")
+                                            }
+                                        }
+                                        .padding(.top, 5)
                                         .frame(maxWidth: .infinity, alignment: .leading)
-                                    Text(String(format: "Price: %@k", String(Int(carDetail.customerPrice / 1000))))
-                                        .font(.subheadline)
-                                        .foregroundColor(.secondary)
-                                        .frame(maxWidth: .infinity, alignment: .leading)
-                                    HStack(spacing: 2) {
-                                        let numOfStars: Int = carDetail.rating
-                                        ForEach(1...numOfStars, id: \.self) { _ in
-                                            Image(systemName: "star.fill")
+                                        .foregroundColor(.yellow)
+                                    }
+                                    .padding(.leading, 10)
+                                }
+                                if item.selected {
+                                    VStack {
+                                        let pros: [String] = item.detail.prosList
+                                        let cons: [String] = item.detail.consList
+                                        if pros.isEmpty == false {
+                                            Text("Pros:")
+                                                .font(.title2)
+                                                .foregroundColor(.secondary)
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                            ForEach(pros, id: \.self) { pro in
+                                                HStack {
+                                                    Image(systemName: "circle.fill")
+                                                        .resizable()
+                                                        .frame(maxWidth: 6, maxHeight: 6)
+                                                        .foregroundColor(Color("appPrimary"))
+                                                    Text(pro)
+                                                        .font(.body)
+                                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                                }
+                                                .padding(EdgeInsets(top: 5, leading: 10, bottom: 0, trailing: 0))
+                                            }
+                                        }
+                                        if cons.isEmpty == false {
+                                            Text("Cons:")
+                                                .font(.title2)
+                                                .foregroundColor(.secondary)
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                            ForEach(cons, id: \.self) { con in
+                                                HStack {
+                                                    Image(systemName: "circle.fill")
+                                                        .resizable()
+                                                        .frame(maxWidth: 6, maxHeight: 6)
+                                                        .foregroundColor(Color("appPrimary"))
+                                                    Text(con)
+                                                        .font(.body)
+                                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                                }
+                                                .padding(EdgeInsets(top: 5, leading: 10, bottom: 0, trailing: 0))
+                                            }
                                         }
                                     }
-                                    .padding(.top, 5)
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .foregroundColor(.yellow)
+                                    .padding(EdgeInsets(top: 5, leading: 10, bottom: 0, trailing: 0))
                                 }
-                                .padding(.leading, 10)
                             }
-                            .padding(EdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0))
+                            .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
+                            .onTapGesture {
+                                presenter.select(item)
+                            }
                         }
                     }
                     .frame(maxHeight: .infinity)
@@ -81,7 +128,6 @@ struct CarListView: View {
                 
             }
         }
-
     }
 }
 
