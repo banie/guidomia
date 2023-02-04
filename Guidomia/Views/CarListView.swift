01cc624d882d650701ed39f592da4a29f65f05b2
Banie Setijoso <banie.setijoso@gmail.com>
2023-02-04 01:49:05 -0500

    Add the top header assets, place it, and fix the list alignment

--- Guidomia/Views/CarListView.swift
+++ Guidomia/Views/CarListView.swift
@@ -12,19 +12,46 @@ struct CarListView: View {
     
     var body: some View {
         NavigationView {
-            List {
-                let carList = presenter.carList
-                ForEach(carList) { carDetail in
-                    Text(carDetail.model).font(.title)
+            ZStack {
+                Color("appPrimary")
+                    .opacity(0.1)
+                    .ignoresSafeArea()
+                VStack{
+                    Rectangle()
+                        .frame(height: 0)
+                        .background(Color("appPrimary"))
+                    Image("topHeader")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(
+                            minWidth: 0,
+                            maxWidth: .infinity,
+                            minHeight: 0,
+                            maxHeight: 200
+                        )
+                    List {
+                        let carList = presenter.carList
+                        ForEach(carList) { carDetail in
+                            Text(carDetail.model).font(.title)
+                        }
+                    }
+                    .frame(maxHeight: .infinity)
+                    .listStyle(.plain)
                 }
+                .frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity,
+                    alignment: .topLeading
+                )
             }
-            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarTitleDisplayMode(.large)
             .toolbar {
-                ToolbarItem(placement: .principal) {
+                ToolbarItem(placement: .navigationBarLeading) {
                     Text("Guidomia")
                         .font(.largeTitle)
-                        .foregroundColor(.black)
+                        .foregroundColor(.white)
                 }
+                
             }
         }
 
