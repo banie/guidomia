bfa1c086de6e2f5829fdb7bea5f5732fa2e41895
Banie Setijoso <banie.setijoso@gmail.com>
2023-02-04 16:09:01 -0500

    Cleaned the pro and cons list because there's empty and whitespace characters there that are messing up my life

--- Guidomia/Presenters/CarListPresenter.swift
+++ Guidomia/Presenters/CarListPresenter.swift
@@ -7,15 +7,50 @@
 
 import Foundation
 
-class CarListPresenter {
+class CarListPresenter: ObservableObject {
     
-    var carList: [CarDetail]
+    @Published var items: [ListItem]
     
     init() {
         do {
-            carList = try GetCarListInteractor().getListOfCarDetails() ?? []
+            let carList = try GetCarListInteractor().getListOfCarDetails() ?? []
+            items = carList.map { carDetail in
+                let cleanedPros = carDetail.prosList.map { pro in
+                    pro.trimmingCharacters(in: .whitespacesAndNewlines)
+                }.filter { pro in
+                    pro.isEmpty == false
+                }
+                let cleanedCons = carDetail.consList.map { con in
+                    con.trimmingCharacters(in: .whitespacesAndNewlines)
+                }.filter { con in
+                    con.isEmpty == false
+                }
+                
+                let cleanCarDetail = CarDetail(model: carDetail.model, make: carDetail.make, customerPrice: carDetail.customerPrice, marketPrice: carDetail.marketPrice, prosList: cleanedPros, consList: cleanedCons, rating: carDetail.rating)
+                
+                return ListItem(detail: cleanCarDetail)
+            }
+            if var firstItem = items.first {
+                firstItem.selected = true
+                items[0] = firstItem
+            }
         } catch {
-            carList = []
+            items = []
         }
     }
+    
+    func select(_ item: ListItem) {
+        for i in 0..<items.count {
+            items[i].selected = items[i].id == item.id ? true : false
+        }
+    }
+}
+
+struct ListItem: Identifiable {
+    var id: String {
+        detail.id
+    }
+    
+    let detail: CarDetail
+    var selected = false
 }
